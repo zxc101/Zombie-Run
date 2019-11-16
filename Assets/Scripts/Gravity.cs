be7a5d2c5b870a95c4608247b92122c478f702e9
zxc101 <zxc101@mail.ru>
2019-11-16 14:50:15 +0300

    Создана гравитация

---
+++ Assets/Scripts/Gravity.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gravity : MonoBehaviour
+{
+    const float speed = 9.81f;
+    private void Update()
+    {
+        Physics.gravity = -transform.up * speed;
+    }
+}
