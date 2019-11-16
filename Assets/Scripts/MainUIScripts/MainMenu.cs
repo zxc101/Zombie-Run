cac3bd2b7fcfe015418a771c7087aafdb52a6287
Кирилл Князев <rizorpolder@gmail.com>
2019-11-16 15:08:00 +0300

    AddedUI

---
+++ Assets/Scripts/MainUIScripts/MainMenu.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenu : MonoBehaviour
+{
+    public void Play()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1 );
+    }
+
+    public void Quit()
+    {
+        //Saving?
+        Application.Quit();
+    }
+}
