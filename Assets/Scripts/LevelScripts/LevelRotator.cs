3e69b59ac2931cf3b495b5f232811751596cf824
Кирилл Князев <rizorpolder@gmail.com>
2019-11-16 23:55:47 +0300

    Spinner Q+E

---
+++ Assets/Scripts/LevelScripts/LevelRotator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelRotator : MonoBehaviour
+{
+   public Transform target;
+
+   void Update()
+   {
+       if (Input.GetKeyDown(KeyCode.Q))
+       {
+           Rotate(-90);
+       }
+
+       if (Input.GetKeyDown(KeyCode.E))
+       {
+           Rotate(90);
+       }
+    }
+
+   void Rotate(int angle)
+   {
+       transform.RotateAround(target.transform.position,Vector3.left,angle);
+   }
+
+}
