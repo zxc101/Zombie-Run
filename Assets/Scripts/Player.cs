1e398de0efb14a6c70859d9cc480f7c23ef2ee78
zxc101 <zxc101@mail.ru>
2019-11-17 15:19:11 +0300

    добавил звуки персонажа

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,19 +1,70 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using UnityEngine.Audio;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] private float timeForImmortality;
+    [SerializeField] private float hp;
+    [SerializeField] private List<Transform> items;
+	private AudioSource _takeDamage;
+	[SerializeField]
+	private Sound[] sounds;
 
-    // Start is called before the first frame update
-    void Start()
+    private float timer;
+
+    public float HP => hp;
+	private Sound _idleSound;
+
+	void Awake()
+	{
+		foreach(Sound s in sounds)
+		{
+			s.source = gameObject.AddComponent<AudioSource>();
+			s.source.clip = s.Clip;
+			s.source.volume = s.Volume;
+			s.source.pitch = s.Pitch;
+		}
+	}
+	
+	void Start()
+	{
+		_idleSound = Array.Find(sounds, sound => sound.Name == "Idle");
+		_idleSound.source.loop = true;
+		_idleSound.source.Play();
+	}
+    private void FixedUpdate()
     {
-        
+        timer -= Time.fixedDeltaTime;
+		_takeDamage = GetComponent<AudioSource>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Damage(float power)
     {
-        
+        if (timer <= 0)
+        {
+            hp -= 1;
+            timer = Time.timeScale + timeForImmortality;
+			Play("Damaged");
+        }
     }
+
+    public void Heal(float power)
+    {
+		Play("Healed");
+        hp += power;
+    }
+
+    public void GetItem(Transform item)
+    {
+		Play("TakeItem");
+        items.Add(item);
+    }
+	public void Play(string name)
+	{
+		Sound s = Array.Find(sounds, sound => sound.Name == name);
+		s.source.Play();
+	}
 }
