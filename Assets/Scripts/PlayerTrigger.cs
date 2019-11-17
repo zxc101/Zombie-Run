2cea42edf5947be868f72e5a988d3e4402e9cc1c
zxc101 <zxc101@mail.ru>
2019-11-17 15:19:11 +0300

    Замёрджил ветку BlocksMove

---
+++ Assets/Scripts/PlayerTrigger.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTrigger : MonoBehaviour
+{
+    [SerializeField] private EPlayerItemTypes type;
+    [SerializeField] private float power;
+
+    private void OnTriggerStay(Collider collider)
+    {
+        Player player = GetPlayer(collider.transform).GetComponent<Player>();
+        if (player.tag == "Player")
+        {
+            if (player != null)
+            {
+                if(type == EPlayerItemTypes.Enemy)
+                {
+                    player.Damage(power);
+                }
+                else if (type == EPlayerItemTypes.Heal)
+                {
+                    player.Heal(power);
+                    transform.gameObject.SetActive(false);
+                }
+                else
+                {
+                    player.GetItem(transform);
+                    transform.gameObject.SetActive(false);
+                }
+            }
+        }
+    }
+
+    private Transform GetPlayer(Transform player)
+    {
+        if (player.parent == null)
+        {
+            return player;
+        }
+        else
+        {
+            return GetPlayer(player.parent);
+        }
+    }
+}
---
+++ Assets/Scripts/ScrollerMove.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollerMove : MonoBehaviour
+{
+    [SerializeField] private Transform endPosition;
+    [SerializeField] private float speed;
+    [SerializeField] private float stopTime;
+
+    private Vector3 direction => endPosition.position - transform.position;
+
+    private void Start()
+    {
+        StartCoroutine(Move());
+    }
+
+    private IEnumerator Move()
+    {
+        yield return new WaitForSeconds(stopTime);
+        while (true)
+        {
+            transform.position += direction * speed * Mathf.Cos(Time.fixedTime * speed) * Time.fixedDeltaTime;
+            yield return new WaitForFixedUpdate();
+        }
+    }
+}
