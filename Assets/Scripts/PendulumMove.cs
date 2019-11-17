fdcc62b299dc0fba597d9d3845f26225cef75c46
zxc101 <zxc101@mail.ru>
2019-11-17 05:24:22 +0300

    Создал передвигающиеся блоки

---
+++ Assets/Scripts/PendulumMove.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PendulumMove : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private float stopTime;
+    [SerializeField] private float angle;
+    
+    private void Start()
+    {
+        StartCoroutine(Move());
+    }
+
+    private IEnumerator Move()
+    {
+        yield return new WaitForSeconds(stopTime);
+        while (true)
+        {
+            Quaternion target = Quaternion.Euler(transform.rotation.eulerAngles.x,
+                                                 transform.rotation.eulerAngles.y,
+                                                 transform.rotation.eulerAngles.z + angle * Mathf.Cos(Time.fixedTime));
+            
+            transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.fixedDeltaTime * speed);
+            yield return new WaitForFixedUpdate();
+        }
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,16 +4,35 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] private float timeForImmortality;
+    [SerializeField] private float hp;
+    [SerializeField] private List<Transform> items;
 
-    // Start is called before the first frame update
-    void Start()
+    private float timer;
+
+    public float HP => hp;
+
+    private void FixedUpdate()
     {
-        
+        timer -= Time.fixedDeltaTime;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Damage(float power)
     {
-        
+        if (timer <= 0)
+        {
+            hp -= 1;
+            timer = Time.timeScale + timeForImmortality;
+        }
+    }
+
+    public void Heal(float power)
+    {
+        hp += power;
+    }
+
+    public void GetItem(Transform item)
+    {
+        items.Add(item);
     }
 }
